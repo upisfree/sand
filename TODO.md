ad3f39f78ed125e0fddf867d6f81f204ac7a131a
upisfree <upisfree@outlook.com>
2015-07-07 01:28:30 +0600

    Add builds

--- TODO.md
+++ TODO.md
@@ -1,5 +1,6 @@
 # TODO
 - [ ] shared lib for client & server
 - [ ] concat libs in one file
+- [ ] **REWRITE GULPFILE**
 - [ ] shared code for tasks in gulpfile
 - [ ] gulpfile in CoffeeScript
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
     livereload = require('gulp-livereload');
 
 var path =
@@ -22,7 +23,7 @@ gulp.task('client', function()
                    path.src.client + '*/*.coffee',
                    path.src.client + 'start.coffee'])
              .pipe(coffee({ bare: true }).on('error', gutil.log))
-             .pipe(concat('client.js'))
+             .pipe(concat('client.dev.js'))
              .pipe(gulp.dest(path.build))
              .pipe(livereload());
 });
@@ -33,8 +34,35 @@ gulp.task('server', function()
                    path.src.server + '*/*.coffee',
                    path.src.server + 'start.coffee'])
              .pipe(coffee({ bare: true }).on('error', gutil.log))
-             .pipe(concat('server.js'))
+             .pipe(concat('server.dev.js'))
              .pipe(gulp.dest(path.build));
+});
+
+gulp.task('build-client', function()
+{
+  return gulp.src([path.src.client + 'globals.coffee',
+                   path.src.client + '*/*.coffee',
+                   path.src.client + 'start.coffee'])
+             .pipe(coffee({ bare: true }).on('error', gutil.log))
+             .pipe(concat('client.js'))
+             .pipe(uglify())
+             .pipe(gulp.dest(path.build));
+});
+
+gulp.task('build-server', function()
+{
+  return gulp.src([path.src.server + 'globals.coffee',
+                   path.src.server + '*/*.coffee',
+                   path.src.server + 'start.coffee'])
+             .pipe(coffee({ bare: true }).on('error', gutil.log))
+             .pipe(concat('server.js'))
+             .pipe(uglify())
+             .pipe(gulp.dest(path.build));
+});
+
+gulp.task('build', function()
+{
+  gulp.run('build-client', 'build-server');
 });
 
 gulp.task('default', function()
